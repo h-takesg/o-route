46eacabe8b05822c3ee01830a0a24577ff5af085
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-04-16 23:35:36 +0900

    任意の地点で回転させられるようになった

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,6 +1,6 @@
 import Konva from "konva"
-import { KeyboardEvent, KeyboardEventHandler, useCallback, useEffect, useRef, useState } from "react"
-import { Layer, Rect, Stage, Image } from "react-konva"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
+import { Layer, Stage, Image, Circle, Group } from "react-konva"
 import useImage from "use-image"
 
 type Props = {
@@ -16,52 +16,79 @@ const MapImage = () => {
 }
 
 function Canvas({size}: Props) {
-  console.log("render Konva Stage");
   const stageRef = useRef<Konva.Stage>(null)
+  const groupRef = useRef<Konva.Group>(null)
   const scaleBy = 1.1;
-  const [rotation, setRotation] = useState(0)
+  const rotateBy = 0.02;
   const [ctrlKey, setCtrlKey] = useState(false)
 
-  const keydownHandler = useCallback((event: KeyboardEvent) => {
+  // Canvasの座標で中心を指定しGroupをdegree回転させる
+  const rotateAt = (x: number, y: number, degree: number) => {
+    const stage = stageRef.current;
+    const group = groupRef.current;
+    const angleRadian = degree * Math.PI / 180;
+
+    if (stage === null) return;
+    if (group === null) return;   
+
+    const newX =  x +
+                  (group.x() - x) * Math.cos(angleRadian) - 
+                  (group.y() - y) * Math.sin(angleRadian);
+    const newY =  y +
+                  (group.x() - x) * Math.sin(angleRadian) +
+                  (group.y() - y) * Math.cos(angleRadian);
+    group.position({x: newX, y: newY});
+    group.rotation((group.rotation() + degree)%360);
+  }
+
+  const handleMousemove = (event: Konva.KonvaEventObject<MouseEvent>) => {
+    if(event.evt.ctrlKey && event.evt.buttons > 0) {
+      console.log(groupRef.current?.getPosition())
+    }
+  }
+
+  const handleKeydown = (event: KeyboardEvent) => {
     if (event.ctrlKey) {
       setCtrlKey(true)
     }
-  }, [])
+  }
 
-  const keyupHandler = useCallback((event: KeyboardEvent) => {
+  const handleKeyup = (event: KeyboardEvent) => {
     if (!event.ctrlKey) {
       setCtrlKey(false)
     }
-  }, [])
+  }
 
-  const wheelHandler = (event: Konva.KonvaEventObject<WheelEvent>) => {
+  const handleWheel = (event: Konva.KonvaEventObject<WheelEvent>) => {
     event.evt.preventDefault()
-    if (stageRef.current !== null) {
+    const stage = stageRef.current
+    const group = groupRef.current
+
+    if (stage !== null && group !== null) {
       if (event.evt.ctrlKey) {
-        setRotation(rotation + 1 % 360)
+        rotateAt(size.width / 2, size.height / 2, event.evt.deltaY * rotateBy);
       } else {
-        const stage = stageRef.current
-        const oldScale = stage.scaleX()
-        const { x: pointerX, y: pointerY } = stage.getPointerPosition();
+        const oldScale = group.scaleX()
+        const { x: pointerX, y: pointerY } = stage.getPointerPosition()!;
         const mousePointTo = {
-          x: (pointerX - stage.x()) / oldScale,
-          y: (pointerY - stage.y()) / oldScale,
+          x: (pointerX - group.x()) / oldScale,
+          y: (pointerY - group.y()) / oldScale,
         }
         const newScale = event.evt.deltaY > 0 ? oldScale / scaleBy : oldScale * scaleBy;
-        stage.scale({ x: newScale, y: newScale});
+        group.scale({ x: newScale, y: newScale});
         const newPos = {
           x: pointerX - mousePointTo.x * newScale,
           y: pointerY - mousePointTo.y * newScale,
         }
-        stage.position(newPos);
+        group.position(newPos);
         stage.batchDraw();
       } 
     }
   }
 
   useEffect(() => {
-    document.addEventListener("keydown", keydownHandler, false)
-    document.addEventListener("keyup", keyupHandler, false)
+    document.addEventListener("keydown", handleKeydown, false)
+    document.addEventListener("keyup", handleKeyup, false)
   }, [])
 
   return (
@@ -69,12 +96,18 @@ function Canvas({size}: Props) {
       height={size.height}
       width={size.width}
       ref={stageRef}
-      rotation={rotation}
-      onWheel={wheelHandler}
-      draggable={!ctrlKey}
+      onMouseMove={handleMousemove}
+      onWheel={handleWheel}
     >
-      <Layer>
-        <MapImage />
+      <Layer
+      >
+        <Group
+        ref={groupRef}
+        draggable={!ctrlKey}
+        >
+          <MapImage />
+        </Group>
+        <Circle fill="red" radius={10} x={size.width/2} y={size.height/2} />
       </Layer>
     </Stage>
   )
