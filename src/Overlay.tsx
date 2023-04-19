7c59e9268aabdd295097080f032ca69a3a229609
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-04-19 15:09:31 +0900

    クリアボタンと画像選択ボタンを追加

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -3,6 +3,7 @@ import { Dispatch, SetStateAction, useState } from "react"
 import OpenWithIcon from '@mui/icons-material/OpenWith';
 import ModeIcon from '@mui/icons-material/Mode';
 import ClearIcon from '@mui/icons-material/Clear';
+import ImageIcon from '@mui/icons-material/Image';
 import {FaEraser} from 'react-icons/fa';
 import { Mode } from "./types";
 
@@ -20,32 +21,60 @@ function Overlay({mode, setMode}: Props) {
 
   return (
     <>
-      <ToggleButtonGroup
-        orientation="vertical"
-        value={mode}
-        exclusive
-        size="large"
-        color="primary"
+      <Box
         sx={{
           position: "fixed",
           top: "0",
           margin: "1rem",
-          opacity: "100",
-          background: "white",
-          borderRadius: "4px"
+          display: "flex",
+          flexDirection: "column"
         }}
-        onChange={handleModeChange}
       >
-        <ToggleButton value="move">
-          <OpenWithIcon fontSize="large" />
+        <ToggleButtonGroup
+          orientation="vertical"
+          value={mode}
+          exclusive
+          size="large"
+          color="primary"
+          sx={{
+            background: "white",
+            borderRadius: "4px",
+            boxShadow: 20,
+            margin: "1rem"
+          }}
+          onChange={handleModeChange}
+        >
+          <ToggleButton value="move">
+            <OpenWithIcon fontSize="large" />
+          </ToggleButton>
+          <ToggleButton value="draw">
+            <ModeIcon fontSize="large" />
+          </ToggleButton>
+          <ToggleButton value="erase">
+            <FaEraser style={{fontSize: "1.7rem"}}/>
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <ToggleButton value="allclear" sx={{
+          background: "white",
+          margin: "0 1rem",
+          boxShadow: 20,
+          ":hover": {
+            background: "#f5f5f5"
+          }
+        }}>
+          <ClearIcon fontSize="large"/>
         </ToggleButton>
-        <ToggleButton value="draw">
-          <ModeIcon fontSize="large" />
+        <ToggleButton value="load" sx={{
+          background: "white",
+          margin: "1rem",
+          boxShadow: 20,
+          ":hover": {
+            background: "#f5f5f5"
+          }
+        }}>
+          <ImageIcon fontSize="large"/>
         </ToggleButton>
-        <ToggleButton value="erase">
-          <FaEraser style={{fontSize: "1.7rem"}}/>
-        </ToggleButton>
-      </ToggleButtonGroup>
+      </Box>
     </>
   )
 }
