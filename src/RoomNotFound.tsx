37bec9b019a0b0549d5d8b1fa25c47a3c29cb4ae
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-04-24 06:00:07 +0900

    RoomNotFoundをリッチにした

--- src/RoomNotFound.tsx
+++ src/RoomNotFound.tsx
@@ -1,8 +1,27 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 function RoomNotFound() {
+  const navigate = useNavigate();
+
+  const gotoHome = () => {
+    navigate("/");
+  }
+
   return (
-    <>
-      Room not found.
-    </>
+    <div style={{
+      maxWidth: "35rem",
+      margin: "1rem auto",
+      padding: "0 2rem"
+    }}>
+      <h1 style={{color: "#ff3333"}}>ルームが見つかりませんでした</h1>
+      <p>URLが間違っているかルームの有効期限が切れているかもしれません．</p>
+      <div style={{width: "100%"}}>
+        <Button variant="contained" style={{margin: "0 auto"}} onClick={gotoHome}>
+          Home
+        </Button>
+      </div>
+    </div>
   )
 }
 
