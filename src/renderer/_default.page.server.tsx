57fae8264f14575035b60c63dc3b45834211a1e3
h-takesg <82633007+h-takesg@users.noreply.github.com>
2023-07-14 18:50:50 +0000

    google向けタイトルを設定

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -25,8 +25,16 @@ async function render(pageContext: PageContextBuiltIn) {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>o-route</title>
-        <meta name="description" content="オリエンテーリングの地図読みに使えるオンラインホワイトボード"/>
+        <script type="application/ld+json">
+          {
+            "@context" : "https://schema.org",
+            "@type" : "WebSite",
+            "name" : "O-Route",
+            "url" : "https://o-route.web.app/"
+          }
+        </script>
+        <title>O-Route | オリエンテーリング向けオンラインホワイトボード</title>
+        <meta name="description" content="オリエンテーリングの地図読みに使えるオンラインホワイトボード．読み込んだ画像を回転させながらルートを書き込めます．"/>
       </head>
       <body>
         <div id="root">${dangerouslySkipEscape(pageHtml)}</div>
